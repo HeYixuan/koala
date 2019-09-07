cc9e7e4b2d26b7f1f3daa2a940359e5978493e7e
HeYixuan <939531410@qq.com>
2019-09-07 11:52:37 +0800

    oauth2授权

--- koala-common/koala-data-scope/src/main/java/org/igetwell/common/data/scope/interceptor/DataScopeInterceptor.java
+++ koala-common/koala-data-scope/src/main/java/org/igetwell/common/data/scope/interceptor/DataScopeInterceptor.java
@@ -1,23 +1,35 @@
 package org.igetwell.common.data.scope.interceptor;
 
+import com.baomidou.mybatisplus.core.toolkit.PluginUtils;
 import com.baomidou.mybatisplus.extension.handlers.AbstractSqlParserHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlCommandType;
+import org.apache.ibatis.mapping.StatementType;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
 import org.igetwell.common.data.scope.annotation.DataScopeAuth;
 import org.igetwell.common.data.scope.datascope.DataScope;
+import org.igetwell.common.data.scope.enums.DataScopeEnum;
 import org.igetwell.common.uitls.ClassUtils;
 import org.igetwell.common.uitls.SpringContextHolder;
+import org.igetwell.oauth.security.KoalaUser;
+import org.igetwell.oauth.security.SpringSecurityUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
 
 
 /**
@@ -34,83 +46,80 @@ public class DataScopeInterceptor extends AbstractSqlParserHandler implements In
 	@Override
     @SneakyThrows
 	public Object intercept(Invocation invocation) {
-//		/*KoalaUser koalaUser = SpringSecurityUtils.getUser();
-//		if (koalaUser == null) {
-//			invocation.proceed();
-//			//throw new Exception("DataScopeInterceptor auto datascope, set up security details true");
-//		}
-//
-//		StatementHandler statementHandler = PluginUtils.realTarget(invocation.getTarget());
-//		MetaObject metaObject = SystemMetaObject.forObject(statementHandler);
-//		this.sqlParser(metaObject);
-//		// 先判断是不是SELECT操作
-//		MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("delegate.mappedStatement");
-//		if (!SqlCommandType.SELECT.equals(mappedStatement.getSqlCommandType())
-//				|| StatementType.CALLABLE == mappedStatement.getStatementType()) {
-//			return invocation.proceed();
-//		}
-//
-//		//查找注解中包含DataAuth类型的参数
-//		DataScopeAuth dataAuth = findDataAuthAnnotation(mappedStatement);
-//
-//		*//*//*/注解为空并且数据权限方法名未匹配到,则放行
-//		String mapperId = mappedStatement.getId();
-//		String className = mapperId.substring(0, mapperId.lastIndexOf("."));
-//		String mapperName = ClassUtils.getShortName(className);
-//		String methodName = mapperId.substring(mapperId.lastIndexOf(".") + 1);
-//		boolean mapperSkip = dataScopeProperties.getMapperKey().stream().noneMatch(methodName::contains)
-//				|| dataScopeProperties.getMapperExclude().stream().anyMatch(mapperName::contains);
-//		if (dataAuth == null && mapperSkip) {
-//			return invocation.proceed();
-//		}*//*
-//		if (dataAuth == null && koalaUser == null) {
-//			return invocation.proceed();
-//		}
-//
-//		BoundSql boundSql = (BoundSql) metaObject.getValue("delegate.boundSql");
-//		String originalSql = boundSql.getSql();
-//		String column = "dept_id";
-//		List<Long> deptIds = new ArrayList<>();
-//		// 优先获取赋值数据
-//		if (StringUtils.isNotEmpty(koalaUser.getDeptId())) {
-//
-//			List<String> roleIdList = koalaUser.getAuthorities()
-//					.stream().map(GrantedAuthority::getAuthority)
-//					.collect(Collectors.toList());
-//
-//			System.err.println(roleIdList);
-//
-//			//Integer dsType = jdbcTemplate.queryForObject("SELECT ds_type FROM sys_role where id = ?", new Object[]{koalaUser.getRoleId()}, Integer.class);
-//			Map<String,Object> params = jdbcTemplate.queryForMap("SELECT ds_type, ds_scope FROM sys_role where id = ?", new Object[]{koalaUser.getRoleId()});
-//			int dsType = (int) params.get("ds_type");
-//
-//			// 查询全部
-//			if (DataScopeEnum.ALL.getType() == dsType) {
-//				return invocation.proceed();
-//			}
-//			// 自定义
-//			if (DataScopeEnum.CUSTOM.getType() == dsType) {
-//			    String dsScope = (String) params.get("ds_scope");
-//                deptIds.addAll(Arrays.stream(dsScope.split(","))
-//                        .map(Long::parseLong).collect(Collectors.toList()));
-//			}
-//			if (DataScopeEnum.OWN.getType() == dsType) {
-//				deptIds.add(koalaUser.getId());
-//			}
-//			// 只查询本级
-//			if (DataScopeEnum.OWN_DEPT.getType() == dsType) {
-//				deptIds.add(Long.valueOf(koalaUser.getDeptId()));
-//			}
-//			// 查询本级及其下级
-//			if (DataScopeEnum.OWN_DEPT_CHILD.getType() == dsType) {
-//				List<Long> deptIdList = jdbcTemplate.queryForList("SELECT child FROM sys_dept_relation WHERE parent = ?", Long.class, koalaUser.getDeptId());
-//				deptIds.addAll(deptIdList);
-//			}
-//
-//		}
-//		String join = StringUtils.join(deptIds, ",");
-//		originalSql = "select * from (" + originalSql + ") temp_data_scope where temp_data_scope." + column + " in (" + join + ")";
-//		metaObject.setValue("delegate.boundSql.sql", originalSql);*/
+
+		/*StatementHandler statementHandler = PluginUtils.realTarget(invocation.getTarget());
+		MetaObject metaObject = SystemMetaObject.forObject(statementHandler);
+		this.sqlParser(metaObject);
+		// 先判断是不是SELECT操作
+		MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("delegate.mappedStatement");
+		if (!SqlCommandType.SELECT.equals(mappedStatement.getSqlCommandType())
+				|| StatementType.CALLABLE == mappedStatement.getStatementType()) {
+			return invocation.proceed();
+		}
+
+		//查找注解中包含DataAuth类型的参数
+		DataScopeAuth dataAuth = findDataAuthAnnotation(mappedStatement);
+
+*//*
+		//注解为空并且数据权限方法名未匹配到,则放行
+		String mapperId = mappedStatement.getId();
+		String className = mapperId.substring(0, mapperId.lastIndexOf("."));
+		String mapperName = ClassUtils.getShortName(className);
+		String methodName = mapperId.substring(mapperId.lastIndexOf(".") + 1);
+		boolean mapperSkip = dataScopeProperties.getMapperKey().stream().noneMatch(methodName::contains)
+				|| dataScopeProperties.getMapperExclude().stream().anyMatch(mapperName::contains);
+		if (dataAuth == null && mapperSkip) {
+			return invocation.proceed();
+		}*//*
+		KoalaUser koalaUser = SpringSecurityUtils.getUser();
+		if (dataAuth == null && koalaUser == null) {
+			return invocation.proceed();
+			//throw new Exception("DataScopeInterceptor auto datascope, set up security details true");
+		}
+
+		BoundSql boundSql = (BoundSql) metaObject.getValue("delegate.boundSql");
+		String originalSql = boundSql.getSql();
+		String column = "dept_id";
+		List<Long> deptIds = new ArrayList<>();
+		// 优先获取赋值数据
+		if (StringUtils.isNotEmpty(koalaUser.getDeptId())) {
+
+			List<String> roleIdList = koalaUser.getAuthorities()
+					.stream().map(GrantedAuthority::getAuthority)
+					.collect(Collectors.toList());
+
+			System.err.println(roleIdList);
+
+			Map<String,Object> params = jdbcTemplate.queryForMap("SELECT ds_type, ds_scope FROM sys_role where id = ?", new Object[]{koalaUser.getRoleId()});
+			int dsType = (int) params.get("ds_type");
+
+			// 查询全部
+			if (DataScopeEnum.ALL.getType() == dsType) {
+				return invocation.proceed();
+			}
+			// 自定义
+			if (DataScopeEnum.CUSTOM.getType() == dsType) {
+			    String dsScope = (String) params.get("ds_scope");
+                deptIds.addAll(Arrays.stream(dsScope.split(","))
+                        .map(Long::parseLong).collect(Collectors.toList()));
+			}
+			if (DataScopeEnum.OWN.getType() == dsType) {
+				deptIds.add(koalaUser.getId());
+			}
+			// 只查询本级
+			if (DataScopeEnum.OWN_DEPT.getType() == dsType) {
+				deptIds.add(Long.valueOf(koalaUser.getDeptId()));
+			}
+			// 查询本级及其下级
+			if (DataScopeEnum.OWN_DEPT_CHILD.getType() == dsType) {
+				List<Long> deptIdList = jdbcTemplate.queryForList("SELECT child FROM sys_dept_relation WHERE parent = ?", Long.class, koalaUser.getDeptId());
+				deptIds.addAll(deptIdList);
+			}
+
+		}
+		String join = StringUtils.join(deptIds, ",");
+		originalSql = "select * from (" + originalSql + ") temp_data_scope where temp_data_scope." + column + " in (" + join + ")";
+		metaObject.setValue("delegate.boundSql.sql", originalSql);*/
 		return invocation.proceed();
 	}
 
