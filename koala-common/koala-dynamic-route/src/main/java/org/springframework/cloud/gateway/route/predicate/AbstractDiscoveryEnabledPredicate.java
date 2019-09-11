342b47e233eb27eda6190e91355a9b4f8cb65391
HeYixuan <939531410@qq.com>
2019-09-11 18:15:22 +0800

    Feign增强处理完成，开始做网关处理

--- koala-common/koala-dynamic-route/src/main/java/org/springframework/cloud/gateway/route/predicate/AbstractDiscoveryEnabledPredicate.java
+++ koala-common/koala-dynamic-route/src/main/java/org/springframework/cloud/gateway/route/predicate/AbstractDiscoveryEnabledPredicate.java
@@ -1,20 +1,3 @@
-/*
- *    Copyright (c) 2018-2025, lengleng All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- * Neither the name of the pig4cloud.com developer nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- * Author: lengleng (wangiegie@gmail.com)
- */
-
 package org.springframework.cloud.gateway.route.predicate;
 
 import com.netflix.loadbalancer.AbstractServerPredicate;
@@ -26,7 +9,6 @@ import org.springframework.lang.Nullable;
 /**
  * 过滤服务
  *
- * @author L.cm
  */
 public abstract class AbstractDiscoveryEnabledPredicate extends AbstractServerPredicate {
 
--- koala-common/koala-dynamic-route/src/main/java/org/springframework/cloud/gateway/route/support/RedisRouteDefinitionWriter.java
+++ koala-common/koala-dynamic-route/src/main/java/org/springframework/cloud/gateway/route/support/RedisRouteDefinitionWriter.java
@@ -1,20 +1,3 @@
-/*
- *    Copyright (c) 2018-2025, lengleng All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- * Neither the name of the pig4cloud.com developer nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- * Author: lengleng (wangiegie@gmail.com)
- */
-
 package org.springframework.cloud.gateway.route.support;
 
 import lombok.AllArgsConstructor;
@@ -35,7 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author jekey
  * redis 保存路由信息，优先级比配置文件高
  */
 @Slf4j
