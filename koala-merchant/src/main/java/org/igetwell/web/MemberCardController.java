b4c8bc2eae77706e0ce8f6d6f0f79e5a93b826f8
HeYixuan <15517551511@126.com>
2019-10-27 12:55:50 +0800

    微信会员卡

--- koala-merchant/src/main/java/org/igetwell/web/MemberCardController.java
+++ koala-merchant/src/main/java/org/igetwell/web/MemberCardController.java
@@ -5,9 +5,11 @@ import org.igetwell.service.impl.MerchantCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/memberCard")
+@RestController
+@RequestMapping("/memberCard")
 public class MemberCardController extends BaseController {
 
     @Autowired
